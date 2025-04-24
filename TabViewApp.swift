a19be9576d80b0614df621f0e3e6a050c9a2b9aa
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2025-04-24 08:55:43 -0300

    Create  TabViewApp.swift

---
+++ TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Turma02-8 on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
