a76a767c659bc2816c2ba388b04fd9165f90cf88
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2025-04-24 08:57:41 -0300

    Create Tela4.View.swift

---
+++ Tela4.View.swift
@@ -0,0 +1,16 @@
+//
+//  Tela4.View.swift
+//  TabView
+//
+
+import SwiftUI
+
+struct Tela4_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tela4_View()
+}
